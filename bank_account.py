a5c14efc7acabb5e414b2531f29918a7e295d9b5
John Felechner <jfelechner@comcast.net>
2021-12-05 13:28:48 -0600

    updates

--- bank_account.py
+++ bank_account.py
@@ -1,31 +1,68 @@
 
+#Create a BankAccount class with the attributes interest rate and balance
+
+
+#Add a deposit method to the BankAccount class
+
+
+#Add a withdraw method to the BankAccount class
+
+
+#Add a display_account_info method to the BankAccount class
+
+
+#Add a yield_interest method to the BankAccount class
+
+
+#Create 2 accounts
+
+
+#To the first account, make 3 deposits and 1 withdrawal, then yield interest and display the account's info all in one line of code (i.e. chaining)
+
+
+#To the second account, make 2 deposits and 4 withdrawals, then yield interest and display the account's info all in one line of code (i.e. chaining)
+
+
+#NINJA BONUS: use a classmethod to print all instances of a Bank Account's info
+
+
 class BankAccount:
 
-
-    def __init__(self, name): 
+    accounts = []
+    def __init__(self, name, int_rate, balance): 
         self.name = name
-        self.int_rate = 1.5
-        self.balance = 0
+        self.int_rate = int_rate
+        self.balance = balance
+        BankAccount.accounts.append(self)
 
     def deposit(self, amount):
         self.balance += amount
         return self
 
     def withdrawl(self, amount):
-        self.balance -= amount
+        if(self.balance - amount) >= 0:
+            self.balance -= amount
+        else:
+            print("Insufficient Funds: Charging a $5 fee")
+            self.balance -= 5
         return self
 
     def yield_interest(self):
         if self.balance > 0:
-            self.intrest_balance = self.balance * self.int_rate
-            return self
-
-    def display_account_info(self):
-        print(f"Bank Account: {self.name}, Balance: {self.intrest_balance}")
+            self.balance += (self.balance * self.int_rate)
         return self
 
-robert = BankAccount("123")
-mike = BankAccount("678")
+    def display_account_info(self):
+        print(f"Bank Account: {self.name}, Balance: {self.balance}")
+        return self
+
+    @classmethod
+    def print_all_accounts(cls):
+        for account in cls.accounts:
+            account.display_account_info()
+
+robert = BankAccount("123", 1.5, 0)
+mike = BankAccount("678", 1.5, 0)
 
 
 robert.deposit(2000).deposit(1300).deposit(750).withdrawl(325).yield_interest().display_account_info()
@@ -33,3 +70,4 @@ robert.deposit(2000).deposit(1300).deposit(750).withdrawl(325).yield_interest().
 mike.deposit(3500).deposit(2900).withdrawl(400).withdrawl(175).withdrawl(400).withdrawl(175).yield_interest().display_account_info()
 
 
+BankAccount.print_all_accounts()
